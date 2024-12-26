9a910470019cc6c6be5662c1f21bf862be6fb995
Samnang Dam <kcattor@gmail.com>
2024-12-25 21:09:03 -0800

    Add header and menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from './components/header/app-header.component';
+import { AppMenuComponent } from './components/menu/app-menu.component';
+
+const components = [AppHeaderComponent, AppMenuComponent];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, components],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
